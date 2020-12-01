5cd27ae96d25811ef7e02b17f76c704496abdf27
Holly Cummins <cumminsh@uk.ibm.com>
2020-12-01 17:36:57 +0000

    Update author in node metadata, and front page identity

--- src/components/Post/Author.js
+++ src/components/Post/Author.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import config from "../../../content/meta/config";
-import avatar from "../../images/jpg/avatar.jpg";
+import avatar from "../../images/jpg/HollyCummins_0219_rt.jpg";
 
 const Author = props => {
   const { note, theme } = props;
