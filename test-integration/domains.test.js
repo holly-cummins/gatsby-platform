5c7bf2e5885b2a430abf33c382bfbe5dc765526e
Holly Cummins <cumminsh@uk.ibm.com>
2020-12-07 21:31:14 +0000

    Restructure to nest around domain names

--- test-integration/domains.test.js
+++ test-integration/domains.test.js
@@ -5,11 +5,16 @@ validateMediumRedirect = async () => {
   await expect(page.waitForXPath('//*[contains(text(), "Holly K Cummins")]')).resolves.toBeTruthy();
 };
 
-describe("http", () => {
-  describe("root", () => {
-    describe("hollycummins.com", () => {
+validateNoRedirect = async name => {
+  await expect(page.url()).toMatch(name);
+};
+
+describe("hollycummins.com", () => {
+  const domain = "hollycummins.com";
+  describe("http", () => {
+    describe("apex", () => {
       beforeAll(async () => {
-        await page.goto("http://hollycummins.com");
+        await page.goto("http://" + domain);
       });
 
       it('should be Hollys page"', async () => {
@@ -17,19 +22,9 @@ describe("http", () => {
       });
     });
 
-    describe("hollycummins.fun", () => {
+    describe("www", () => {
       beforeAll(async () => {
-        await page.goto("http://hollycummins.fun");
-      });
-
-      it('should be Hollys page"', async () => {
-        await validateMediumRedirect();
-      });
-    });
-
-    describe("hollycummins.dev", () => {
-      beforeAll(async () => {
-        await page.goto("http://hollycummins.dev");
+        await page.goto("http://www." + domain);
       });
 
       it('should be Hollys page"', async () => {
@@ -37,11 +32,14 @@ describe("http", () => {
       });
     });
   });
+});
 
-  describe("www", () => {
-    describe("www.hollycummins.com", () => {
+describe("hollycummins.fun", () => {
+  const domain = "hollycummins.fun";
+  describe("http", () => {
+    describe("apex", () => {
       beforeAll(async () => {
-        await page.goto("http://www.hollycummins.com");
+        await page.goto("http://" + domain);
       });
 
       it('should be Hollys page"', async () => {
@@ -49,19 +47,33 @@ describe("http", () => {
       });
     });
 
-    describe("www.hollycummins.fun", () => {
+    describe("www", () => {
       beforeAll(async () => {
-        await page.goto("http://www.hollycummins.fun");
+        await page.goto("http://www." + domain);
       });
 
       it('should be Hollys page"', async () => {
         await validateMediumRedirect();
       });
     });
+  });
+});
 
-    describe("www.hollycummins.dev", () => {
+describe("hollycummins.dev", () => {
+  const domain = "hollycummins.dev";
+  describe("http", () => {
+    describe("apex", () => {
       beforeAll(async () => {
-        await page.goto("http://www.hollycummins.dev");
+        await page.goto("http://" + domain);
+      });
+
+      it('should be Hollys page"', async () => {
+        await validateMediumRedirect();
+      });
+    });
+    describe("www", () => {
+      beforeAll(async () => {
+        await page.goto("http://www." + domain);
       });
 
       it('should be Hollys page"', async () => {
