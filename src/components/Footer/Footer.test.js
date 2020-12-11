444111dea9da81bdcaade995a8e1054a8fdcf1bf
Holly Cummins <cumminsh@uk.ibm.com>
2020-12-11 12:32:21 +0000

    Test that the footer doesn't mangle the injected html

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -14,4 +14,10 @@ describe("Footer", () => {
     render(<Footer html="" theme={themeObjectFromYaml} />);
     expect(screen.findByRole("contentinfo")).toBeTruthy();
   });
+
+  it("renders the html argument", () => {
+    const text = "this is the displayed text";
+    render(<Footer html={`<h2>${text}</h2>`} theme={themeObjectFromYaml} />);
+    expect(screen.findByText(text)).toBeTruthy();
+  });
 });
