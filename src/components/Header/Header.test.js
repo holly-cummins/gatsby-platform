396df5670b6425da0ec6b0e4ed6426dbe4077c3a
Holly Cummins <cumminsh@uk.ibm.com>
2020-12-11 12:52:00 +0000

    Fix failure which only shows in the CI

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../header";
+import Header from ".";
 
 import themeObjectFromYaml from "../../theme/theme.yaml";
 
