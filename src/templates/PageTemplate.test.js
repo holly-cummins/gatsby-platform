63c39b23fc03331f6be85ffd8a13904604b11bfc
Holly Cummins <cumminsh@uk.ibm.com>
2021-01-08 19:22:14 +0000

    Add skeleton test for PageTemplate

---
+++ src/templates/PageTemplate.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import PageTemplate from "./PageTemplate";
+
+import { ThemeContext } from "../layouts";
+
+import themeObjectFromYaml from "../theme/theme.yaml";
+
+// @see https://testing-library.com/docs/react-testing-library/setup#custom-render
+const renderWithTheme = ui => {
+  return render(<ThemeContext.Provider value={themeObjectFromYaml}>{ui}</ThemeContext.Provider>);
+};
+
+describe("PageTemplate", () => {
+  it("renders the title", () => {
+    const html = "html";
+    const slug = "sluggeroo";
+    const title = "some page";
+    const page = {
+      frontmatter: { category: "test-stuff", title },
+      fields: { source: "pages", slug },
+      html
+    };
+    const facebook = { to: "remove-soon" };
+    const data = {
+      page,
+      site: {
+        siteMetadata: { facebook }
+      }
+    };
+
+    const tree = renderWithTheme(<PageTemplate data={data} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+});
