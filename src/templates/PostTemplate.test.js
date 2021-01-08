852a1b617ac3d938b20e2258d45e67026ad9402c
Holly Cummins <cumminsh@uk.ibm.com>
2021-01-08 20:06:38 +0000

    Skeleton test for post template

---
+++ src/templates/PostTemplate.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import PostTemplate from "./PostTemplate";
+
+import { ThemeContext } from "../layouts";
+
+import themeObjectFromYaml from "../theme/theme.yaml";
+
+// @see https://testing-library.com/docs/react-testing-library/setup#custom-render
+const renderWithTheme = ui => {
+  return render(<ThemeContext.Provider value={themeObjectFromYaml}>{ui}</ThemeContext.Provider>);
+};
+
+describe("PostTemplate", () => {
+  it("renders the title", () => {
+    const authorNote = "author-note";
+    const slug = "sluggeroo";
+    const title = "some post";
+    const post = {
+      frontmatter: { category: "test-stuff", title, author: "bob" },
+      fields: { prefix: "prefixeroo", slug },
+      html: "post-content"
+    };
+    const facebook = { appId: "remove-soon" };
+    const data = {
+      post,
+      site: {
+        siteMetadata: { facebook }
+      },
+      authornote: { html: authorNote }
+    };
+
+    const tree = renderWithTheme(<PostTemplate data={data} pageContext={{}} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+});
