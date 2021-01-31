37974fef8310e9ea00834adf175d907b3acd0990
Holly Cummins <cumminsh@uk.ibm.com>
2021-01-31 14:21:17 +0000

    Add styling and logos to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,6 +7,12 @@ import Headline from "../components/Article/Headline";
 import Seo from "../components/Seo";
 import config from "../../content/meta/config";
 
+import LinkedInIcon from "!svg-react-loader!../images/svg-icons/linkedin.svg";
+import TwitterIcon from "!svg-react-loader!../images/svg-icons/twitter.svg";
+import GitHubIcon from "!svg-react-loader!../images/svg-icons/github.svg";
+
+const icons = { linkedin: <LinkedInIcon />, twitter: <TwitterIcon />, github: <GitHubIcon /> };
+
 const ContactPage = props => {
   const {
     data: {
@@ -17,11 +23,6 @@ const ContactPage = props => {
   } = props;
 
   const platforms = config.authorSocialLinks;
-  const platformLinks = platforms.map(platform => (
-    <li key={platform.name}>
-      <a href={platform.url}>{platform.display}</a>
-    </li>
-  ));
 
   return (
     <React.Fragment>
@@ -32,7 +33,42 @@ const ContactPage = props => {
               <Headline title="Contact" theme={theme} />
             </header>
             <p>You can message me on social media.</p>
-            <ul>{platformLinks}</ul>
+            <ul>
+              {platforms.map(platform => (
+                <li key={platform.name}>
+                  <div>
+                    <div className="icon">{icons[platform.name]}</div>
+                    <a href={platform.url}>{platform.display}</a>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            {/* --- STYLES --- */}
+            <style jsx>
+              {`
+                ul {
+                  margin: ${theme.space.stack.m};
+                  padding: ${theme.space.m};
+                  list-style: none;
+                }
+                li {
+                  padding: ${theme.space.xs} 0;
+                  font-size: ${theme.font.size.m};
+                  line-height: ${theme.font.lineHeight.l};
+                }
+                div {
+                  display: flex;
+                  justify-content: flex-begin;
+                  align-items: center;
+                }
+                .icon {
+                  padding: ${theme.space.m};
+                }
+                .icon :global(svg) {
+                  height: 40px;
+                }
+              `}
+            </style>
           </Article>
         )}
       </ThemeContext.Consumer>
