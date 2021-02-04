9560ea35cd29c1cb1817625672aface3a5e04079
Holly Cummins <cumminsh@uk.ibm.com>
2021-02-04 17:28:42 +0000

    Trivial unit test for menu

---
+++ src/components/Menu/Menu.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import Menu from "./Menu";
+
+import theme from "../../theme/theme.yaml";
+
+describe("Menu", () => {
+  const fixed = true;
+  const path = "homepage";
+  const width = 10;
+  const loaded = true;
+  const pages = [];
+
+  beforeEach(() => {
+    const tree = render(
+      <Menu
+        path={path}
+        fixed={fixed}
+        screenWidth={width}
+        fontLoaded={loaded}
+        pages={pages}
+        theme={theme}
+      />
+    );
+  });
+
+  it("includes a search item", () => {
+    expect(screen.getByText("Search")).toBeTruthy();
+  });
+});
