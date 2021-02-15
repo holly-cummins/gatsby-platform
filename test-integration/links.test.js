6767864278250cd5a95eff45f278959e60a29c8a
Holly Cummins <cumminsh@uk.ibm.com>
2021-02-15 21:45:38 +0000

    Work around link checker hang by not checking the problem site

--- test-integration/links.test.js
+++ test-integration/links.test.js
@@ -20,12 +20,16 @@ describe("site links", () => {
             "https://twitter.com/holly_cummins",
             "https://www.linkedin.com/in/holly-k-cummins/",
             "https://www.manning.com/books/enterprise-osgi-in-action",
-            "https://www.klarkteknik.com/product.html?modelCode=P0DPA"
+            "https://www.klarkteknik.com/product.html?modelCode=P0DPA",
+            "https://www.cnbc.com/2016/05/03/this-blind-man-is-running-a-155-mile-ultra-marathon-with-the-help-of-an-ibm-app.html",
+            "http://www.element14.com/community/thread/26532/l/quick-start-of-pcduino-without-a-hdmi-monitor-and-serial-debug-cable?displayFullThread=true"
           ] // We know these links are good and we want to not hit the rate limiters since they appear everywhere
           // Manning and KlarkTeknik seem to have a bot-blocker, which is annoying, since the link seems likely to break
+          // The element14 link causes a hang
         },
         {
           error: error => {
+            console.log("Rejecting promise in sitechecker: ", error);
             reject(error);
           },
           link: (result, customData) => {
