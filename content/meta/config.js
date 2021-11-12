bb3748597fc0bc654dafef27e8944fc3bc509f9b
Holly Cummins <cumminsh@uk.ibm.com>
2021-11-12 17:31:24 +0000

    Add in needed metadata and boilerplate

---
+++ content/meta/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  siteTitle: "Ducky Devine", // <title>
+  shortSiteTitle: "Ducky Devine", // <title> ending for posts and pages
+  siteDescription: "There's not really any content here but the template code is nice.",
+  siteUrl: "http://duckydevine.com",
+  pathPrefix: "",
+  siteImage: "preview.png",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "Ducky Devine",
+  authorTwitterAccount: "ducky_devine", // TODO remove this which is only used by Seo.js and is duplication
+
+  /* info */
+  headerTitle: "Ducky Devine",
+  headerSubTitle: "",
+
+  /* manifest.json */
+  manifestName: "Ducky Devine",
+  manifestShortName: "DuckyBlog", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    { name: "twitter", url: "https://twitter.com/ducky_devine", display: "@ducky_devine" },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/ducky-devine/",
+      display: "Ducky Devine"
+    },
+    { name: "github", url: "https://github.com/ducky-devine", display: "ducky-devine" },
+    { name: "medium", url: "https://ducky-devine.medium.com", display: "Ducky Devine" }
+  ]
+};
---
+++ content/parts/footnote.md
@@ -0,0 +1,6 @@
+---
+title: footnote
+---
+
+- built by [holly cummins](https://www.linkedin.com/in/holly-k-cummins/)
+- follow holly on [twitter](https://twitter.com/holly_cummins)
