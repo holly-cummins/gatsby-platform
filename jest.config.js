f1bad2d3b48b850566d43439e0387777c6c37bf1
Holly Cummins <cumminsh@uk.ibm.com>
2021-12-01 17:54:55 +0000

    Auto-import jest-dom

--- jest.config.js
+++ jest.config.js
@@ -15,5 +15,6 @@ module.exports = {
     __PATH_PREFIX__: ``
   },
   testURL: `http://localhost`,
-  setupFiles: [`<rootDir>/loadershim.js`]
+  setupFiles: [`<rootDir>/loadershim.js`],
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.js"]
 };
