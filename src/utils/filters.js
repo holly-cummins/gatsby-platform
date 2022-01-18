dc417aab51595042d4b48ad06acb3afefc203c59
Holly Cummins <cumminsh@uk.ibm.com>
2022-01-18 09:16:31 +0000

    Refactor to extract common code

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,22 +1,28 @@
-exports.generateFilter = otherFilter => {
-  // Do not create draft post files in production.
-  let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development";
+// Do not create draft post files in production.
+// This is dynamic rather than a constant for ease of testing
+const isProd = () => {
+  const activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development";
   // Be less chatty when testing
   if (activeEnv != "test") {
     console.log(`Using environment config: '${activeEnv}'`);
   }
+  const isProd = activeEnv == "production";
+  return isProd;
+};
+
+exports.generateFilter = otherFilter => {
   let draftsFilters = { fields: { slug: { ne: "" } } };
-  if (activeEnv == "production") {
+
+  if (isProd()) {
     draftsFilters = { fields: { slug: { ne: "" }, prefix: { ne: "draft" } } };
   }
   const filters = { ...draftsFilters, ...otherFilter };
 
   return filters;
 };
+
 exports.filterOutDrafts = edges => {
-  // TODO refactor this out
-  const activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development";
-  if (activeEnv == "production") {
+  if (isProd()) {
     edges = edges.filter(edge =>
       edge.node && edge.node.fields ? edge.node.fields.prefix != "draft" : true
     );
