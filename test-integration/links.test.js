52906907c18991639f58c86f713a5b1ebe5230e7
Holly Cummins <hcummins@redhat.com>
2022-04-19 22:50:05 +0100

    Work around link checker 404s on twitter by switching link checking library. Local run is not clean, but it's better, so let's see what happens in the build.

--- test-integration/links.test.js
+++ test-integration/links.test.js
@@ -1,73 +1,63 @@
 jest.setTimeout(180 * 1000);
 
-const { SiteChecker } = require("broken-link-checker");
+const link = require("linkinator");
 
 describe("site links", () => {
+  console.log("link is", link);
   const deadExternalLinks = [];
   const deadInternalLinks = [];
 
   beforeAll(async () => {
-    const promise = new Promise((resolve, reject) => {
-      const siteChecker = new SiteChecker(
-        {
-          excludeInternalLinks: false,
-          excludeExternalLinks: false,
-          retryHeadCodes: [404, 405],
-          maxSocketsPerHost: 1,
-          filterLevel: 0,
-          acceptedSchemes: ["http", "https"],
-          userAgent:
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0", // Twitter is fussy about the user agent and will not serve to bots
-          excludedKeywords: [
-            "https://twitter.com/ducky_devine",
-            "https://www.linkedin.com/in/ducky-devine/",
-            "https://www.linkedin.com/in/holly-k-cummins/",
-            "https://www.manning.com/books/d-is-for-duck", // known 404, DO NOT SEARCH AND REPLACE
-            "https://github.com/ducky-devine", // known 404, DO NOT SEARCH AND REPLACE
-            "https://ducky-devine.medium.com", // known 404, DO NOT SEARCH AND REPLACE
-            "https://www.manning.com/books/enterprise-osgi-in-action",
-            "https://www.klarkteknik.com/product.html?modelCode=P0DPA",
-            "https://www.cnbc.com/2016/05/03/this-blind-man-is-running-a-155-mile-ultra-marathon-with-the-help-of-an-ibm-app.html",
-            "http://www.element14.com/community/thread/26532/l/quick-start-of-pcduino-without-a-hdmi-monitor-and-serial-debug-cable?displayFullThread=true",
-            "https://www.uk.mercer.com/our-thinking/the-gender-pay-gap-in-uk-tech-sector.html"
-          ] // We know these links are good and we want to not hit the rate limiters since they appear everywhere
-          // Manning and KlarkTeknik seem to have a bot-blocker, which is annoying, since the link seems likely to break
-          // NOTE: The Manning, Medium, and gitgub D is For Duck is fictitious by design, so exclude them
-          // DO NOT search and replace these with your own name
-          // The element14 link causes a hang
-        },
-        {
-          error: error => {
-            console.log("Rejecting promise in sitechecker: ", error);
-            reject(error);
-          },
-          link: result => {
-            if (result.broken) {
-              if (
-                result.http.response &&
-                ![undefined, 200].includes(result.http.response.statusCode)
-              ) {
-                const description = `${result.url.original} => ${result.http.response.statusCode} (${result.http.response.statusMessage}) on ${result.base.original}`;
-                if (result.internal) {
-                  if (!deadInternalLinks.includes(description)) {
-                    deadInternalLinks.push(description);
-                  }
-                } else {
-                  if (!deadExternalLinks.includes(description)) {
-                    deadExternalLinks.push(description);
-                  }
-                }
-              }
-            }
-          },
-          end: () => {
-            resolve();
+    // create a new `LinkChecker` that we'll use to run the scan.
+    const checker = new link.LinkChecker();
+
+    // Respond to the beginning of a new page being scanned
+    checker.on("pagestart", url => {
+      console.log(`Scanning ${url}`);
+    });
+
+    // After a page is scanned, check out the results!
+    checker.on("link", result => {
+      if (result.state == "BROKEN") {
+        //   console.log(result);
+        const description = `${result.url} => ${result.status} (${result.statusText}) on ${result.parent}`;
+        if (result.internal) {
+          // TODO this will always be false as linkinator does not give us this
+          if (!deadInternalLinks.includes(description)) {
+            deadInternalLinks.push(description);
+          }
+        } else {
+          if (!deadExternalLinks.includes(description)) {
+            deadExternalLinks.push(description);
           }
         }
-      );
-      siteChecker.enqueue("http://localhost:9000/");
+      }
     });
-    await promise;
+
+    const excludedLinks = [
+      "https://twitter.com/ducky_devine",
+      "https://www.linkedin.com/in/ducky-devine/",
+      "https://www.linkedin.com/in/holly-k-cummins/",
+      "https://www.manning.com/books/d-is-for-duck", // known 404, DO NOT SEARCH AND REPLACE
+      "https://github.com/ducky-devine", // known 404, DO NOT SEARCH AND REPLACE
+      "https://ducky-devine.medium.com", // known 404, DO NOT SEARCH AND REPLACE
+      "https://www.manning.com/books/enterprise-osgi-in-action",
+      "https://www.klarkteknik.com/product.html?modelCode=P0DPA",
+      "https://www.cnbc.com/2016/05/03/this-blind-man-is-running-a-155-mile-ultra-marathon-with-the-help-of-an-ibm-app.html",
+      "http://www.element14.com/community/thread/26532/l/quick-start-of-pcduino-without-a-hdmi-monitor-and-serial-debug-cable?displayFullThread=true",
+      "https://www.uk.mercer.com/our-thinking/the-gender-pay-gap-in-uk-tech-sector.html"
+    ]; // We know these links are good and we want to not hit the rate limiters since they appear everywhere
+    // Manning and KlarkTeknik seem to have a bot-blocker, which is annoying, since the link seems likely to break
+    // NOTE: The Manning, Medium, and gitgub D is For Duck is fictitious by design, so exclude them
+    // DO NOT search and replace these with your own name
+    // The element14 link causes a hang
+
+    // Go ahead and start the scan! As events occur, we will see them above.
+    const result = await checker.check({
+      path: "http://localhost:9000",
+      recurse: true,
+      linksToSkip: excludedLinks
+    });
   });
 
   it("internal links should all resolve", async () => {
