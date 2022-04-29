d7737cc517ae3e388848134636f3056d4060a588
Holly Cummins <hcummins@redhat.com>
2022-04-29 11:46:09 +0100

    Remove link from excludes, since it works with linkinator

--- test-integration/links.test.js
+++ test-integration/links.test.js
@@ -52,8 +52,7 @@ describe("site links", () => {
       "https://www.manning.com/books/enterprise-osgi-in-action",
       "https://www.klarkteknik.com/product.html?modelCode=P0DPA",
       "https://www.cnbc.com/2016/05/03/this-blind-man-is-running-a-155-mile-ultra-marathon-with-the-help-of-an-ibm-app.html",
-      "http://www.element14.com/community/thread/26532/l/quick-start-of-pcduino-without-a-hdmi-monitor-and-serial-debug-cable?displayFullThread=true",
-      "https://www.uk.mercer.com/our-thinking/the-gender-pay-gap-in-uk-tech-sector.html"
+      "http://www.element14.com/community/thread/26532/l/quick-start-of-pcduino-without-a-hdmi-monitor-and-serial-debug-cable?displayFullThread=true"
     ]; // We know these links are good and we want to not hit the rate limiters since they appear everywhere
     // Manning and KlarkTeknik seem to have a bot-blocker, which is annoying, since the link seems likely to break
     // NOTE: The Manning, Medium, and gitgub D is For Duck is fictitious by design, so exclude them
