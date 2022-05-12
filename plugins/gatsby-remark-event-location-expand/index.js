86433248534a0ec48f2e93a90c105acbe1ddb206
Holly Cummins <hcummins@redhat.com>
2022-05-12 22:55:19 +0100

    More debugging in production - see if just calling the geocoder creates issues

--- plugins/gatsby-remark-event-location-expand/index.js
+++ plugins/gatsby-remark-event-location-expand/index.js
@@ -15,19 +15,19 @@ exports.mutateSource = async ({ markdownNode }) => {
   const { location } = frontmatter;
 
   if (location) {
-    const results = null; // await geocoder.geocode(location);
-    if (results && results.length > 0) {
-      // Assume it puts the most likely first
-      const res = results[0];
-      const geography = {};
-      geography.country = res.country;
-      geography.countryCode = res.countryCode;
-      const flag = flags[res.countryCode];
-      if (flag) {
-        const buff = Buffer.from(flag);
-        geography.flag = buff.toString("base64");
-      }
-      // frontmatter.geography = geography;
+    const results = await geocoder.geocode(location);
+    // if (results && results.length > 0) {
+    //   // Assume it puts the most likely first
+    //   const res = results[0];
+    //   const geography = {};
+    //   geography.country = res.country;
+    //   geography.countryCode = res.countryCode;
+    //   const flag = flags[res.countryCode];
+    //   if (flag) {
+    //     const buff = Buffer.from(flag);
+    //     geography.flag = buff.toString("base64");
+    //   }
+    //   frontmatter.geography = geography;
     }
   }
 };
