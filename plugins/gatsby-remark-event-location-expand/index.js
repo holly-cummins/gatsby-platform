8a59023c73b1ef8253e0c948f56fa6331cdd29ad
Holly Cummins <hcummins@redhat.com>
2022-05-13 17:27:05 +0100

    Add diagnostic logging and less conditional structure to try and resolve partial population of flags

--- plugins/gatsby-remark-event-location-expand/index.js
+++ plugins/gatsby-remark-event-location-expand/index.js
@@ -11,32 +11,34 @@ exports.mutateSource = async ({ markdownNode }) => {
   const { frontmatter } = markdownNode;
   const { location } = frontmatter;
 
-  if (location) {
-    try {
-      const geocoder = nodeGeocoder(options);
-      const results = await geocoder.geocode(location);
-      if (results && results.length > 0) {
-        // Assume it puts the most likely first
-        const res = results[0];
-        const geography = {};
-        geography.country = res.country;
-        geography.countryCode = res.countryCode;
-        const flag = flags[res.countryCode];
-        if (flag) {
-          const buff = Buffer.from(flag);
-          geography.flag = buff.toString("base64");
-        }
-        frontmatter.geography = geography;
+  const geography = location ? await makeGeographyDetails(location) : null;
+  if (geography) {
+    console.log("Filling in geography for", frontmatter.title);
+    frontmatter.geography = geography;
+  }
+};
+
+const makeGeographyDetails = async location => {
+  try {
+    const geocoder = nodeGeocoder(options);
+    const results = await geocoder.geocode(location);
+    if (results && results.length > 0) {
+      // Assume it puts the most likely first
+      const res = results[0];
+      const geography = {};
+      geography.country = res.country;
+      geography.countryCode = res.countryCode;
+      const flag = flags[res.countryCode];
+      if (flag) {
+        const buff = Buffer.from(flag);
+        geography.flag = buff.toString("base64");
       }
-    } catch (e) {
-      console.log(
-        "Error getting geography information for",
-        location,
-        'on "',
-        frontmatter.title,
-        '"'
-      );
-      console.log("Error:", e);
+
+      return geography;
     }
+  } catch (e) {
+    console.log("Error getting geography information for", location);
+    console.log("Error:", e);
+    return null;
   }
 };
