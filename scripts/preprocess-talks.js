0cbeff82f9fc2aaa02617da9d258e855a7fd7990
Holly Cummins <hcummins@redhat.com>
2022-05-15 23:05:25 +0100

    Wait for async more accurately and be a bit less optimistic in deciding whether a placeholder download is needed

--- scripts/preprocess-talks.js
+++ scripts/preprocess-talks.js
@@ -35,21 +35,18 @@ const processDirectory = async () => {
         const source = await readFile(markdownPath);
         const frontmatter = metadataParser(source).metadata;
 
-        if (frontmatter.slides) {
-          const thingWeAreWaitingFor = await enrich(frontmatter.slides, path.join(dir, talk));
+        let slideDownload;
+        let videoDownload;
 
-          // Make sure to wait
-          enrichPromises.push(new Promise(resolve => resolve(thingWeAreWaitingFor)));
+        if (frontmatter.slides) {
+          slideDownload = await enrich(frontmatter.slides, path.join(dir, talk), enrichPromises);
         }
 
         if (frontmatter.video) {
-          const thingWeAreWaitingFor = await enrich(frontmatter.video, path.join(dir, talk));
-
-          // Make sure to wait
-          enrichPromises.push(new Promise(resolve => resolve(thingWeAreWaitingFor)));
+          videoDownload = await enrich(frontmatter.video, path.join(dir, talk), enrichPromises);
         }
 
-        if (!frontmatter.slides && !frontmatter.video) {
+        if (!slideDownload && !videoDownload) {
           // Copy in a placeholder
           const newPlaceholderPath = path.join(dir, talk, "placeholder.png");
           const promise = copyFile(placeholderPath, newPlaceholderPath);
@@ -70,8 +67,7 @@ const processDirectory = async () => {
   }
   return Promise.all(enrichPromises);
 };
-const enrich = async (oembedObject, downloadDir) => {
-  const thingsWeAreWaitingFor = [];
+const enrich = async (oembedObject, downloadDir, thingsWeAreWaitingFor) => {
   const url = oembedObject.url;
   const params = {};
 
@@ -94,13 +90,13 @@ const enrich = async (oembedObject, downloadDir) => {
         // Wait for the download to make sure we don't end up with half-files
         const download = await downloadThumbnail(imageUrl, downloadDir);
         thingsWeAreWaitingFor.push(download);
+        return download;
       }
     } else {
       // What should we do if we have an oembed provider and it returns nothing? Cry in the corner?
       console.error(`Got no oembed data for `, url);
     }
   }
-  return thingsWeAreWaitingFor;
 };
 
 const downloadThumbnail = async (imageUrl, thumbnailDir) => {
