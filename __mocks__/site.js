2c0815f1eea3db2f701670d29c0747e44ae1fa45
Holly Cummins <hcummins@redhat.com>
2022-05-18 17:32:23 +0100

    (Slightly) reduce console warnings in test by updating mock to match new image API

--- __mocks__/site.js
+++ __mocks__/site.js
@@ -2,12 +2,27 @@ module.exports = {
   cover: {
     children: [
       {
-        fluid: {
-          base64: "data:image/png;base64,iV==",
-          aspectRatio: 1,
-          src: "/static/somewhere.png",
-          srcSet: "/static/somewhere.png 75w",
-          sizes: "(max-width: 300px) 100vw, 300px"
+        gatsbyImageData: {
+          layout: "constrained",
+          backgroundColor: "#989898",
+          images: {
+            fallback: {
+              src: "/static/89698dfadf0bb3c133af681004f13991/3ee8f/placeholder.png",
+              srcSet:
+                "/static/89698dfadf0bb3c133af681004f13991/7725f/placeholder.png 85w,\n/static/89698dfadf0bb3c133af681004f13991/46c46/placeholder.png170w,\n/static/89698dfadf0bb3c133af681004f13991/3ee8f/placeholder.png 340w",
+              sizes: "(min-width: 340px) 340px, 100vw"
+            },
+            sources: [
+              {
+                srcSet:
+                  "/static/89698dfadf0bb3c133af681004f13991/24300/placeholder.webp 85w,\n/static/89698dfadf0bb3c133af681004f13991/e07c2/placeholder.webp170w,\n/static/89698dfadf0bb3c133af681004f13991/647fa/placeholder.webp 340w",
+                type: "image/webp",
+                sizes: "(min-width: 340px) 340px, 100vw"
+              }
+            ]
+          },
+          width: 800,
+          height: 360
         }
       }
     ]
