86251f0ad6037a334db9e5e34c3440655dfb7f4f
Holly Cummins <hcummins@redhat.com>
2022-11-24 22:00:33 +0000

    Fix console log

--- plugins/gatsby-remark-tweet-processor/gatsby-node.js
+++ plugins/gatsby-remark-tweet-processor/gatsby-node.js
@@ -76,7 +76,6 @@ const cacheTweet = async (id, cacheDir) => {
 
     const tweet = fetchedTweet.data;
     tweet.author = fetchedTweet?.includes?.users.find(user => (user.id = tweet.author_id));
-    console.log("HOLLY author", tweet.author);
 
     const images = fetchedTweet?.includes?.media.map(
       async media =>
