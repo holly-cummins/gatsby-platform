5e7fc4b9a7601243ac178237db4ae366579a7b6a
Holly Cummins <hcummins@redhat.com>
2023-09-13 15:15:31 +0100

    Whitelist failing site

--- test-integration/links.test.js
+++ test-integration/links.test.js
@@ -59,7 +59,9 @@ describe("site links", () => {
       "https://www.klarkteknik.com/product.html?modelCode=P0DPA", // KlarkTeknik seem to have a bot-blocker, which is annoying, since the link seems likely to break
       "https://www.cnbc.com/2016/05/03/this-blind-man-is-running-a-155-mile-ultra-marathon-with-the-help-of-an-ibm-app.html",
       "https://owlcation.com/humanities/Novels-Inspired-by-Dreams", // Seems to have a trigger-happy rate limiter
-      "https://community.element14.com/technologies/open-source-hardware/f/forum/21938/quick-start-of-pcduino-without-a-hdmi-monitor-and-serial-debug-cable" // Does not work will with linkinator
+      "https://community.element14.com/technologies/open-source-hardware/f/forum/21938/quick-start-of-pcduino-without-a-hdmi-monitor-and-serial-debug-cable", // Does not work will with linkinator
+      "https://trishagee.com/", // Can return 0 to some hosts
+      "https://trishagee.com/2014/03/20/atom-to-hugo/"
     ]; // We know these links are good, and we want to not hit the rate limiters since they appear everywhere
     // NOTE: The Manning, Medium, and GitHub D is For Duck is fictitious by design, so exclude them
     // DO NOT search and replace these with your own name
