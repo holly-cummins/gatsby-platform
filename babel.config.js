702bf4e740938fea576f74e182e62e90b5d5be5a
Holly Cummins <hcummins@redhat.com>
2024-05-13 17:34:48 +0100

    Tidy up babel warnings

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    ["@babel/plugin-transform-class-properties", { loose: true }],
     ["@babel/plugin-transform-private-methods", { loose: true }],
     ["@babel/plugin-transform-private-property-in-object", { loose: true }]
   ],
