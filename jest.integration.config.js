d732e768e6058aed79500a8461c7f63cc127b9a3
Holly Cummins <hcummins@redhat.com>
2024-05-13 16:56:59 +0100

    Update linkinator (including CJS to ESM config updates)

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: "jest-puppeteer",
   testPathIgnorePatterns: [".cache"],
-  setupFilesAfterEnv: ["expect-puppeteer"]
+  setupFilesAfterEnv: ["expect-puppeteer"],
+  // Add ESM dependencies into the group in this pattern
+  transformIgnorePatterns: [
+    `<rootDir>/node_modules/(?!(rehype-react|linkinator|srcset|url-exist|is-url-superb|ky-universal|ky|gatsby)/)`
+  ]
 };
