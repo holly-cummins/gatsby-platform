b562de77cfbda5b99dcfb2474a31895542b01d26
Holly Cummins <hcummins@redhat.com>
2024-05-13 10:36:58 +0100

    Tests all passing locally with updated puppeteer API. do not allow dead sites to be deployed (slightly after the horse has bolted)

--- test-integration/frontpage.test.js
+++ test-integration/frontpage.test.js
@@ -1,4 +1,5 @@
 jest.setTimeout(15 * 1000);
+const { setTimeout } = require("node:timers/promises");
 
 const { port } = require("../jest-puppeteer.config").server;
 const { authorName } = require("../src/utils/configger");
@@ -14,12 +15,12 @@ describe("main site", () => {
   });
 
   it("should have the author name on it somewhere", async () => {
-    await expect(page.waitForXPath(`//*[text()="${authorName}"]`)).resolves.toBeTruthy();
+    await expect(page.waitForSelector(`xpath/ //*[text()="${authorName}"]`)).resolves.toBeTruthy();
   });
 
   it("should have the author name on it attached to the metadata for an entry", async () => {
     await expect(
-      page.waitForXPath(`//p[contains(@class, "meta")]/*[text()="${authorName}"]`)
+      page.waitForSelector(`xpath/ //p[contains(@class, "meta")]/*[text()="${authorName}"]`)
     ).resolves.toBeTruthy();
   });
 
@@ -30,35 +31,38 @@ describe("main site", () => {
       async () => {
         // We should only have a search bar if we have algolia configured
         // eslint-disable-next-line jest/no-standalone-expect
-        await expect(page.waitForXPath('//*[text()="Search"]')).resolves.toBeTruthy();
+        await expect(page.waitForSelector('xpath/ //*[text()="Search"]')).resolves.toBeTruthy();
       }
     );
 
     it("should have a Contact option", async () => {
-      await expect(page.waitForXPath('//*[text()="Contact"]')).resolves.toBeTruthy();
+      await expect(page.waitForSelector('xpath/ //*[text()="Contact"]')).resolves.toBeTruthy();
     });
   });
 
   describe("hero mechanism", () => {
     it("should have button to click", async () => {
-      await expect(page.waitForXPath('//*[contains(@aria-label,"scroll")]')).resolves.toBeTruthy();
+      await expect(
+        page.waitForSelector('xpath/ //*[contains(@aria-label,"scroll")]')
+      ).resolves.toBeTruthy();
     });
 
     it("should scroll down when the button is clicked", async () => {
       // Look for a list item which has a class of 'blog-item'
       const selector = '//li[contains(@class, "blog-item")]';
 
-      const tile = await page.waitForXPath(selector);
+      const tile = await page.waitForSelector("xpath/" + selector);
       // Sense check - make sure the thing is there but not visible (we have to use the viewport, not the css visibility)
       await expect(tile.isIntersectingViewport()).resolves.toBeFalsy();
 
-      const scrollButton = await page.waitForXPath('//*[contains(@aria-label,"scroll")]');
+      const scrollButton = await page.waitForSelector('xpath/ //*[contains(@aria-label,"scroll")]');
       await expect(scrollButton.isIntersectingViewport()).resolves.toBeTruthy();
 
       // Click, which should scroll
       await scrollButton.evaluate(but => but.click());
       // Nasty hack; wait for some scrolling to happen
-      await page.waitFor(2000);
+      await setTimeout(2000);
+
       // Now the button should be out of view
       await expect(scrollButton.isIntersectingViewport()).resolves.toBeFalsy();
 
--- test-integration/talks.test.js
+++ test-integration/talks.test.js
@@ -12,15 +12,17 @@ describe("main site", () => {
     });
 
     it("should be possible from the front page", async () => {
-      await expect(page.waitForXPath('//*[text()="Talks"]')).resolves.toBeTruthy();
+      await expect(page.waitForSelector('xpath/ //*[text()="Talks"]')).resolves.toBeTruthy();
     });
 
     it("should bring up a talks page", async () => {
-      const talksLink = await page.waitForXPath('//a[text()="Talks"]');
+      const talksLink = await page.waitForSelector('xpath/ //a[text()="Talks"]');
       talksLink.evaluate(link => link.click());
 
       // Now it should be the talks page
-      await expect(page.waitForXPath('//h1[contains(text(), "talks")]')).resolves.toBeTruthy();
+      await expect(
+        page.waitForSelector('xpath/ //h1[contains(text(), "talks")]')
+      ).resolves.toBeTruthy();
     });
   });
 
@@ -30,7 +32,9 @@ describe("main site", () => {
     });
 
     it("should be a talks page", async () => {
-      await expect(page.waitForXPath('//h1[contains(text(), "talks")]')).resolves.toBeTruthy();
+      await expect(
+        page.waitForSelector('xpath/ //h1[contains(text(), "talks")]')
+      ).resolves.toBeTruthy();
     });
 
     it("should list at least one item", async () => {
@@ -46,15 +50,15 @@ describe("main site", () => {
     it("should have event names", async () => {
       // We don't know the content, but it's a reasonable guess some content matches SomethingCon
       await expect(
-        page.waitForXPath('//div[contains(@class,"event")]//*[contains(text(), "Con")]')
+        page.waitForSelector('xpath/ //div[contains(@class,"event")]//*[contains(text(), "Con")]')
       ).resolves.toBeTruthy();
     });
 
     describe("on hovering over an event name", () => {
       it("should switch event names for short dates", async () => {
         // We don't know the content, but it's a reasonable guess some content matches SomethingCon
-        const oldestCon = await page.waitForXPath(
-          '//div[contains(@class,"event")]//*[contains(text(), "Con")]'
+        const oldestCon = await page.waitForSelector(
+          'xpath///div[contains(@class,"event")]//*[contains(text(), "Con")]'
         );
         oldestCon.hover();
         // Every element should switch to a date
@@ -62,9 +66,12 @@ describe("main site", () => {
         try {
           // Sadly, we cannot use regex selectors in xpath 1, and selecting for text with css is hard
           // So assume at least one of the dates must contain a 0
-          await page.waitForXPath('//div[contains(@class,"event")]//*[contains(text(), "0")]', {
-            timeout: 5 * 1000
-          });
+          await page.waitForSelector(
+            'xpath/ //div[contains(@class,"event")]//*[contains(text(), "0")]',
+            {
+              timeout: 5 * 1000
+            }
+          );
         } catch (e) {
           const main = await page.$("main");
           // Annoyingly this does not have any formatting but I cannot find a more useful output because outerHtml is too busy
@@ -73,8 +80,8 @@ describe("main site", () => {
         }
 
         // Now do a deeper validation of the date text; we could check every node, but one is probably sufficient
-        const date = await page.waitForXPath(
-          '//div[contains(@class,"event")]//*[contains(text(), "0")]',
+        const date = await page.waitForSelector(
+          'xpath///div[contains(@class,"event")]//*[contains(text(), "0")]',
           { timeout: 5 * 1000 }
         );
         expect(date).toBeTruthy();
