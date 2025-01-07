aca3ddb734dd185373b9a900b7027275296c9a2a
Holly Cummins <hcummins@redhat.com>
2025-01-07 20:13:57 +0000

    Re-enable partial hydration

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,3 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-const ReactDOM = require("react-dom/client");
-
-// See https://github.com/gatsbyjs/gatsby/discussions/36232; it would be ideal to get rid of this, but integration tests fail in ci without it
-
-exports.replaceHydrateFunction = () => {
-  return (element, container) => {
-    const root = ReactDOM.createRoot(container);
-    root.render(element);
-  };
-};
