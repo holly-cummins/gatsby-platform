9e15ed10beec8fce58a368d6dc79b660c37d38e4
holly-cummins <hcummins@redhat.com>
2025-01-07 16:02:37 +0000

    Deploying to pages from @ holly-cummins/gatsby-platform@2654c2d8194009d3b524e3a17f3178a3c20dd7a5 🚀

--- sw.js
+++ sw.js
@@ -42,119 +42,11 @@ self.__precacheManifest = [
     "url": "ad7f724d-10f2a8f3e2deb6ddd603.js"
   },
   {
-    "url": "cfc6b023-c3582647336a4c8bb37e.js"
-  },
-  {
-    "url": "ea683af8-0c0caca5331edcff1b88.js"
-  },
-  {
-    "url": "c4169ad2-5df802bc4528722b12a3.js"
-  },
-  {
-    "url": "43a99af2-924120bf4e25c1b69082.js"
-  },
-  {
-    "url": "fc20e1c4-6825e39c18871dde84ea.js"
-  },
-  {
-    "url": "8edc3fe5-0271604a3e2e6026cd20.js"
-  },
-  {
-    "url": "ebc5ba27-30cc5e73e9dc122ff4d6.js"
-  },
-  {
-    "url": "315606f0-7e01a1943a710b468a88.js"
-  },
-  {
-    "url": "10bf5a56-b3ca83d3a00e1254595f.js"
-  },
-  {
-    "url": "ce8576ea-2d50c231ede60c67e4d1.js"
-  },
-  {
-    "url": "98397353-79706a3397f0efd16515.js"
-  },
-  {
-    "url": "b85045ab-d0168d5492a8bd2346c3.js"
-  },
-  {
-    "url": "c2ec1d10-e3aa8306216231b2ec52.js"
-  },
-  {
-    "url": "8f19d780-bd85d98f54b85ad85e2d.js"
-  },
-  {
-    "url": "5ca00d41-4076ae626fb425c3d60a.js"
-  },
-  {
-    "url": "b16bd182-5c101bb785fab2fbfcdb.js"
-  },
-  {
-    "url": "42e09a2c-3a799dd7f11b1cea446c.js"
-  },
-  {
-    "url": "6e92790c-49c9214f9cc73323bcf1.js"
-  },
-  {
-    "url": "a45cf4d1-097b9d1f8d609236e9c4.js"
-  },
-  {
-    "url": "5f6bb6fb-6ef6972df0016780dbfd.js"
-  },
-  {
-    "url": "79eb02f1-0130e31f8183a81bf3eb.js"
-  },
-  {
-    "url": "3c128e86-755cd3568a48fbd1286f.js"
-  },
-  {
-    "url": "ebd65826-6d5936b55c413cbc8eff.js"
-  },
-  {
-    "url": "2f37a77d-65888c7849e42119996c.js"
-  },
-  {
-    "url": "23420901-263f7a263c882d55c141.js"
-  },
-  {
-    "url": "da011f70-f89323504793b756e150.js"
-  },
-  {
-    "url": "262b3caa-e3122fabbb4a80729618.js"
-  },
-  {
-    "url": "0e6451da-9a48c80f7456a0a67d33.js"
-  },
-  {
-    "url": "4119064d-0666644f757f27dc477a.js"
-  },
-  {
-    "url": "e51a3977-59571adc0d3a13de748f.js"
-  },
-  {
-    "url": "6e2d9d0a-0cdc9898b782062faed3.js"
-  },
-  {
-    "url": "761cb607-27c7047e45dee80a366a.js"
-  },
-  {
-    "url": "439716bf-b81cef2d7be393b9d7df.js"
-  },
-  {
-    "url": "2fb713ed-333a2fe1619c9ad485a9.js"
-  },
-  {
-    "url": "1c920f04-f95e9e951c405960416a.js"
-  },
-  {
-    "url": "9a679482-a561d08ba52eb88c50cd.js"
-  },
-  {
-    "url": "app-cdd6850ec072cad7b08a.js"
+    "url": "9929107f-211076842d1c2592854d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "03058d2656e931e41bf41a981109f0fe"
+    "revision": "874451f4f620f4f9e93c0f2c94674b4b"
   },
   {
     "url": "manifest.webmanifest",
@@ -268,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-cdd6850ec072cad7b08a.js`))) {
+  if (!resources || !(await caches.match(`/app-99cbc76a12f32852edfd.js`))) {
     return await fetch(event.request)
   }
 
